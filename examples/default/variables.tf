d7e2f6bca135991cfbf733abca4af0d212b02bca
Marco Antonio Blanco <marcoantonio.blanco@nearform.com>
2025-02-17 13:27:44 +0100

    feat: add default values to example code variables

--- examples/default/variables.tf
+++ examples/default/variables.tf
@@ -1,9 +1,11 @@
 variable "kv_admins" {
   description = "List of admin user IDs for the Key Vault"
   type        = list(string)
+  default     = []
 }
 
 variable "admins_allowed_ips" {
   description = "Mapping of admin names to their allowed IPs"
   type        = map(string)
+  default     = {}
 }
